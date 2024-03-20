bc175efdce1b09b54f2f2ac0b140b3214bfcc9a8
anthozb3x <anthony.beal@clsi.fr>
2024-03-20 23:23:40 +0100

    refactor : remove unused files

--- schema.sql
+++ schema.sql
@@ -41,14 +41,14 @@ INSERT INTO users (username, password) VALUES ('user1@gmail.fr', '0b14d501a59444
 
 
 -- Listes
-INSERT INTO lists (title, created_by) VALUES ('Liste de courses', 1);
-INSERT INTO lists (title, created_by) VALUES ('Liste de tâches', 2);
+-- INSERT INTO lists (title, created_by) VALUES ('Liste de courses', 1);
+-- INSERT INTO lists (title, created_by) VALUES ('Liste de tâches', 2);
 
 -- Items
-INSERT INTO items (list_id, name, quantity, added_by) VALUES (1, 'Pommes', 5, 1);
-INSERT INTO items (list_id, name, quantity, added_by) VALUES (1, 'Bananes', 3, 2);
-INSERT INTO items (list_id, name, quantity, added_by) VALUES (2, 'Nettoyer la maison', 1, 2);
+-- INSERT INTO items (list_id, name, quantity, added_by) VALUES (1, 'Pommes', 5, 1);
+-- INSERT INTO items (list_id, name, quantity, added_by) VALUES (1, 'Bananes', 3, 2);
+-- INSERT INTO items (list_id, name, quantity, added_by) VALUES (2, 'Nettoyer la maison', 1, 2);
 
 -- Invitations
-INSERT INTO invitations (list_id, user_id, status) VALUES (1, 2, 'pending');
-INSERT INTO invitations (list_id, user_id, status) VALUES (2, 1, 'accepted');
+-- INSERT INTO invitations (list_id, user_id, status) VALUES (1, 2, 'pending');
+-- INSERT INTO invitations (list_id, user_id, status) VALUES (2, 1, 'accepted');
